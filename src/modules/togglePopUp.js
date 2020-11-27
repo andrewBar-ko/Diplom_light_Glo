c5c157b24f4aad08318a913efe865d8031d31d4b
andrewBar-ko <andrewbarkovets@gmail.com>
2020-11-27 12:34:35 +0300

    togglePopUp.js): feat animate

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -2,11 +2,29 @@
 'use strict';
 
 const togglePopUp = () => {
+    const popupCall = document.querySelector('.popup-call'),
+        popupDiscount = document.querySelector('.popup-discount'),
+        popupCheck = document.querySelector('.popup-check'),
+        popupConsultation = document.querySelector('.popup-consultation');
     
     const showElem = e => {
         e.style.display = 'block';
+        e.style.opacity = 0;
+        let op = 0;
+        const setOpacity = () => {
+            let opacity;
+                if( op < 1 ) {
+                    opacity = requestAnimationFrame(setOpacity);
+                    op +=0.04;
+                    e.style.opacity = op;
+                } else{
+                    cancelAnimationFrame(opacity);
+                }
+        };
+        setOpacity();
+        
         e.querySelectorAll('input').forEach(input => {
-            input.value = '';
+            input.value = '';          
         });
     };
     const hideElem = e => {
@@ -15,20 +33,21 @@ const togglePopUp = () => {
         }
         e.style.display = 'none';
     };
+    
     //Открытие разных POPUP
     document.body.addEventListener('click', e => {
         const target = e.target;
         if (target.closest('.call-btn')){ //При клике на эту надписи в header и footer 
-            showElem(document.querySelector('.popup-call'));
+            showElem(popupCall);
         }
         if (target.closest('.discount-btn')){ //после нажатия на кнопку “Получить расчет и скидку” 
-            showElem(document.querySelector('.popup-discount'));
+            showElem(popupDiscount);
         }
         if (target.closest('.check-btn')){ //При нажатии на кнопку “Получить чек-лист и скидку”
-            showElem(document.querySelector('.popup-check'));
+            showElem(popupCheck);
         }
         if (target.closest('.consultation-btn')){ //открывается модальное окно с классом popup-consultation
-            showElem(document.querySelector('.popup-consultation'));
+            showElem(popupConsultation);
         }
     });
     //Закрытие POPUP
