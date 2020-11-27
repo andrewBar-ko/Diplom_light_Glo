be534f8a1c446a3ffcbb3e77dd8abc2ffe70846b
andrewBar-ko <andrewbarkovets@gmail.com>
2020-11-28 02:17:49 +0300

    feat(src/modules): feat addMore.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+import addMore from './modules/addMore';
 import togglePopUp from './modules/togglePopUp';
 import sendForm from './modules/sendForm';
 import preolader from './modules/preolader';
@@ -6,4 +7,5 @@ import preolader from './modules/preolader';
 
 togglePopUp();
 sendForm();
+addMore();
 preolader();
