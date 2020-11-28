eb7a54c364d689cbc970bb4753d67e5cae04cc79
andrewBar-ko <andrewbarkovets@gmail.com>
2020-11-28 17:35:45 +0300

    fix(index.html): del preolader and fix addMore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@
 import addMore from './modules/addMore';
 import togglePopUp from './modules/togglePopUp';
 import sendForm from './modules/sendForm';
-import preolader from './modules/preolader';
+
 
 
 togglePopUp();
 sendForm();
 addMore();
-preolader();
